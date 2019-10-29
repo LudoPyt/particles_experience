cfd7a9cf6e9a886f860b09ba00aa20323f73428e
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-10-29 13:03:46 +0100

    add experience

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import config from './config.js'
 
-const title = document.createElement('h1')
-title.innerHTML = 'hello'
-
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app')
-  app.append(title)
 })
